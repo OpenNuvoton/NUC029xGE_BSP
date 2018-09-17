4bdeaad73da17d44a5f3a9647664c5cd4344b298
MS20 WSChang0 <WSChang0@nuvoton.com>
2018-09-17 15:47:02 +0800

    Initialization

---
+++ SampleCode/StdDriver/USBD_HID_Mouse/main.c
@@ -0,0 +1,203 @@
+/******************************************************************************
+ * @file     main.c
+ * @brief
+ *           Demonstrate how to implement a USB mouse device.
+ *           The mouse cursor will move automatically when this mouse device connecting to PC by USB.
+ *
+ * @note
+ * Copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NUC029xGE.h"
+#include "hid_mouse.h"
+
+
+/* If crystal-less is enabled, system won't use any crystal as clock source
+   If using crystal-less, system will be 48MHz, otherwise, system is 72MHz
+*/
+#define CRYSTAL_LESS        1
+#define HIRC48_AUTO_TRIM    0x412   /* Use USB signal to fine tune HIRC 48MHz */
+#define TRIM_INIT           (SYS_BASE+0x118)
+
+void EnableCLKO(uint32_t u32ClkSrc, uint32_t u32ClkDiv)
+{
+    /* CLKO = clock source / 2^(u32ClkDiv + 1) */
+    CLK->CLKOCTL = CLK_CLKOCTL_CLKOEN_Msk | u32ClkDiv;
+
+    /* Enable CLKO clock source */
+    CLK->APBCLK0 |= CLK_APBCLK0_CLKOCKEN_Msk;
+
+    /* Select CLKO clock source */
+    CLK->CLKSEL2 = (CLK->CLKSEL2 & (~CLK_CLKSEL2_CLKOSEL_Msk)) | u32ClkSrc;
+}
+
+
+void SYS_Init(void)
+{
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Enable Internal RC 22.1184 MHz clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to Internal RC and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+#ifndef CRYSTAL_LESS
+    /* Enable external XTAL 12 MHz clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Waiting for external XTAL clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+    /* Set core clock */
+    CLK_SetCoreClock(72000000);
+
+    /* Use HIRC as UART clock source */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UARTSEL_HIRC, CLK_CLKDIV0_UART(1));
+
+    /* Use PLL as USB clock source */
+    CLK_SetModuleClock(USBD_MODULE, CLK_CLKSEL3_USBDSEL_PLL, CLK_CLKDIV0_USB(3));
+
+#else
+    /* Enable Internal RC 48MHz clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRC48EN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRC48STB_Msk);
+
+    /* Switch HCLK clock source to Internal RC and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC48, CLK_CLKDIV0_HCLK(1));
+
+    /* Use HIRC as UART clock source */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UARTSEL_HIRC, CLK_CLKDIV0_UART(1));
+
+    /* Use HIRC48 as USB clock source */
+    CLK_SetModuleClock(USBD_MODULE, CLK_CLKSEL3_USBDSEL_HIRC48, CLK_CLKDIV0_USB(1));
+#endif
+
+    /* Enable module clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_EnableModuleClock(USBD_MODULE);
+
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Set P3 multi-function pins for UART0 RXD and TXD */
+    SYS->GPD_MFPL &= ~(SYS_GPD_MFPL_PD0MFP_Msk | SYS_GPD_MFPL_PD1MFP_Msk);
+    SYS->GPD_MFPL |= SYS_GPD_MFPL_PD0MFP_UART0_RXD | SYS_GPD_MFPL_PD1MFP_UART0_TXD;
+    SYS->GPD_MFPL &= ~(SYS_GPD_MFPL_PD5MFP_CLKO);
+    SYS->GPD_MFPL |= SYS_GPD_MFPL_PD5MFP_CLKO;
+
+    /* Enable CLKO(PD5) for monitor clock. CLKO = clock src/64 Hz
+        000 = Clock source from 4~24 MHz external high speed crystal oscillator (HXT) clock.
+        001 = Clock source from 32.768 kHz external low speed crystal oscillator (LXT) clock.
+        010 = Clock source from HCLK.
+        011 = Clock source from 22.1184 MHz internal high speed RC oscillator (HIRC) clock.
+        100 = Clock source from SOF (USB start of frame event).
+        101 = Clock source from 48 MHz internal high speed RC oscillator (HIRC48) clock.
+    */
+
+    EnableCLKO((5 << CLK_CLKSEL2_CLKOSEL_Pos), 5);
+
+}
+
+void UART0_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init UART                                                                                               */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Reset IP */
+    SYS->IPRST1 |=  SYS_IPRST1_UART0RST_Msk;
+    SYS->IPRST1 &= ~SYS_IPRST1_UART0RST_Msk;
+
+    /* Configure UART0 and set UART0 Baudrate */
+    UART0->BAUD = UART_BAUD_MODE2 | UART_BAUD_MODE2_DIVIDER(__HIRC, 115200);
+    UART0->LINE = UART_WORD_LEN_8 | UART_PARITY_NONE | UART_STOP_BIT_1;
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+    uint32_t u32TrimInit;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    SYS_Init();
+
+    UART0_Init();
+
+    printf("\n");
+    printf("+-----------------------------------------------------+\n");
+    printf("|          NuMicro USB HID Mouse Sample Code          |\n");
+    printf("+-----------------------------------------------------+\n");
+
+    USBD_Open(&gsInfo, HID_ClassRequest, NULL);
+
+    /* Endpoint configuration */
+    HID_Init();
+    USBD_Start();
+
+    /* Enable USB device interrupt */
+    NVIC_EnableIRQ(USBD_IRQn);
+
+#ifdef CRYSTAL_LESS
+    /* Backup default trim */
+    u32TrimInit = M32(TRIM_INIT);
+#endif
+
+    /* Clear SOF */
+    USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+
+    while(1)
+    {
+#ifdef CRYSTAL_LESS
+        /* Start USB trim if it is not enabled. */
+        if((SYS->IRCTCTL1 & SYS_IRCTCTL1_FREQSEL_Msk) != 2)
+        {
+            /* Start USB trim only when SOF */
+            if(USBD->INTSTS & USBD_INTSTS_SOFIF_Msk)
+            {
+                /* Clear SOF */
+                USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+
+                /* Re-enable crystal-less */
+                SYS->IRCTCTL1 = HIRC48_AUTO_TRIM;
+            }
+        }
+
+        /* Disable USB Trim when error */
+        if(SYS->IRCTISTS & (SYS_IRCTISTS_CLKERRIF1_Msk | SYS_IRCTISTS_TFAILIF1_Msk))
+        {
+            /* Init TRIM */
+            M32(TRIM_INIT) = u32TrimInit;
+
+            /* Disable crystal-less */
+            SYS->IRCTCTL1 = 0;
+
+            /* Clear error flags */
+            SYS->IRCTISTS = SYS_IRCTISTS_CLKERRIF1_Msk | SYS_IRCTISTS_TFAILIF1_Msk;
+
+            /* Clear SOF */
+            USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+        }
+#endif
+
+        HID_UpdateMouseData();
+    }
+}
+
+
+
+/*** (C) COPYRIGHT 2016 Nuvoton Technology Corp. ***/
+
